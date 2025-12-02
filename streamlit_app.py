5718df9dcc17d601a1bec029dfeaab299a2ff670
StockOpsLab <ph.le66@gmail.com>
2025-12-02 14:31:13 +0100

    Center Css

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,32 @@
 import streamlit as st
 
+
+
+
+#### CSS
+st.markdown(
+    """
+    <style>
+    .css-1y0tuds {  /* titre principal */
+        text-align: center;
+    }
+    .css-1offfwp h2 {  /* subheader */
+        text-align: center;
+    }
+    .css-1offfwp p {   /* st.write */
+        text-align: center;
+    }
+    /* Ou plus simple : centrer tous les éléments de header */
+    h1, h2, h3, h4 {
+        text-align: center !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+#### CSS
 st.title("📈 StockOpsLab")
 st.subheader("🚧 Application en construction 🚧")
-
 st.write("Bienvenue sur **StockOpsLab** !")
 
 col1, col2, col3 = st.columns([1,2,1])  # colonne centrale plus large
