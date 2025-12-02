0b5ea394a9f083d4e04dc63fa50bfa2b0ffc19fc
StockOpsLab <ph.le66@gmail.com>
2025-12-02 14:21:20 +0100

    Center

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.subheader("🚧 Application en construction 🚧")
 
 st.write("Bienvenue sur **StockOpsLab** !")
 
+col1, col2, col3 = st.columns([1,2,1])  # colonne centrale plus large
+with col2:
+    st.title("📈 StockOpsLab")
+    st.subheader("🚧 Application en construction 🚧")
+    st.write("Bienvenue sur **StockOpsLab** !")
+
