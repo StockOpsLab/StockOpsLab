0f9e72a4386b66d7f82acf7193561909b90a85f6
StockOpsLab <ph.le66@gmail.com>
2025-12-03 09:16:59 +0100

    Key

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import requests
 
 # Remplace par ton URL et ta clé
-url = "https://ymjabtkhikeofdfyltra.supabase.co"
+url = "https://stockopslab.streamlit.app/"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltamFidGtoaWtlb2ZkZnlsdHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NzE2MjEsImV4cCI6MjA4MDI0NzYyMX0.2fdaFWK5oFz405ECG0qhXN3Z2KCjLS54kuA9XCuEfDM"
 
 
