82b78b06c49e4a4a47294ab82f67f17f78eec110
StockOpsLab <ph.le66@gmail.com>
2025-12-03 15:15:07 +0100

    single

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 
 # Ligne titre
-st.markdown("StockOpsLab<BR>")
-st.markdown("Open Source MWS for training<BR>")
-
-# Troisième ligne
-st.markdown("Under construction<BR>")
-
+st.write("test")
 
 
 
