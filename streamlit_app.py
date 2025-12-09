5114a1efbe7707c1ef0975a9f36d3abb8d65722b
StockOpsLab <ph.le66@gmail.com>
2025-12-09 10:45:05 +0100

    Update streamlit_app.py
    supabase

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from supabase import create_client
 # Configuration Supabase
 SUPABASE_URL = "https://ymjabtkhikeofdfyltra.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltamFidGtoaWtlb2ZkZnlsdHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NzE2MjEsImV4cCI6MjA4MDI0NzYyMX0.2fdaFWK5oFz405ECG0qhXN3Z2KCjLS54kuA9XCuEfDM"  
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 headers = {
     "apikey": SUPABASE_KEY,
     "Authorization": f"Bearer {SUPABASE_KEY}",
